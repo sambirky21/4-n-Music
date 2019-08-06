a7cd26a8d7741c5db4beb1e0c180e68ec243982f
Samuel Birky <sbirky21@gmail.com>
2019-08-06 16:11:19 -0500

    well it doesn't work, but the code is there, and no errors

--- src/components/Sessions/SessionList.js
+++ src/components/Sessions/SessionList.js
@@ -1,40 +1,146 @@
 import React, { Component } from "react";
 import "./sessions.css";
 import PracticeCard from "../Cards/PracticeCard"
+import Timer from "../Timer/Timer"
 
 export default class SessionList extends Component {
+
+    state = {
+        time: [],
+        activeTimer: null,
+        isRunning: false,
+        elapsedTime: null
+    };
+
+    componentDidMount() {
+        this.getCardsInSession(this.state.time);
+        // doubt the above would would because its an aray you are passing in....
+        this.intervalID = setInterval(() => this.tick(), 1000);
+        this.getTime();
+      }
+
+      componentWillUnmount() {
+        clearInterval(this.intervalID);
+      }
+
+      getCardsInSession = () => {
+        this.props.PracticeSessionCards
+            .filter( data =>
+                data.userId === parseInt(sessionStorage.getItem("userId")))
+            .map(data => {
+                return this.props.PracticeCards
+                .filter(card =>
+                (data.cardId === card.id))
+            })
+            // return this.setState({ time: card.time })
+    }
+
+    getTime = () => {
+        let timeOfEachSessionCard = 0
+        this.state.time.forEach(cardTime =>{
+          timeOfEachSessionCard += parseInt(cardTime.time)
+          return timeOfEachSessionCard
+      })
+      let hrs = ~~(timeOfEachSessionCard / 3600);
+      let mins = ~~((timeOfEachSessionCard % 3600) / 60);
+      let secs = ~~timeOfEachSessionCard % 60;
+
+      let ret = "";
+
+      if (hrs > 0) {
+          ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
+      }
+
+      ret += "" + mins + ":" + (secs < 10 ? "0" : "");
+      ret += "" + secs;
+      return ret;
+    }
+
+    startTimer = async index => {
+        await this.setState({ activeTimer: index });
+      };
+
+    startNextTimer = () => {
+        this.setState({ activeTimer: this.state.activeTimer + 1 })
+    }
+
+    handleTimer = index => {
+        this.setState(prevState => ({
+          isRunning: !prevState.isRunning
+        }));
+        this.startTimer(index);
+    };
+
+    handleReset = () => {
+        this.setState(prevState => {
+          let resetCardTime = [...prevState.time];
+          resetCardTime[prevState.activeTimer].elapsedTime =
+            resetCardTime[prevState.activeTimer].time;
+
+          return {
+            time: resetCardTime
+          };
+        });
+    };
+
+    tick = () => {
+        // reduce time by 1 second by creating a copy of the exercise object in state and overwriting the elapsed time every second.
+        if (this.state.isRunning) {
+          this.setState(prevState => {
+            const cardTime = [...prevState.time];
+            cardTime[prevState.activeTimer].elapsedTime -= 1;
+
+            return {
+              time: cardTime
+            };
+          });
+          //Timer audio and stop functionality
+          if (
+            this.state.time.length === this.state.activeTimer + 1 &&
+            this.state.time[this.state.activeTimer].elapsedTime === 0
+          ) {
+            this.setState({ isRunning: false });
+            this.handleReset();
+          }
+          //   when countdown finishes
+          //   reset isRunning bool
+          //   reset clock
+          else if (this.state.time[this.state.activeTimer].elapsedTime <= 0) {
+            // this.setState({ isRunning: false });
+            this.handleReset();
+            this.startNextTimer();
+          }
+        }
+      };
+
+
+
     render() {
     // console.log("sessions 1", this.props.Sessions)
     return (
 
       <React.Fragment>
         <header className="flex center">Practice Session</header>
-        <div className="stopwatch border">
-        <h4>Stopwatch</h4>
-        <h3>This will be the title of current card countdown</h3>
-        <span className="stopwatch-time">This will be the time of current card</span>
-        <br />
-        <button
-          onClick={() =>
-            null
-          }
-          type="button"
-          className="btn btn-secondary btn-sm stopwatch_button start"
-        >
-          { "Start"
-              /* {this.props.isRunning && this.props.activeTimer === this.props.index
-            ? "Pause"
-            : "Start"} */}
-        </button>
-        <button
-        disabled={null}
-          onClick={null}
-          type="button"
-          className={`${"btn btn-secondary btn-sm stopwatch_button reset"}`}
-       >
-          Reset
-        </button>
-      </div>
+
+        <section>
+            <article className="timer">
+            {this.state.time.map((time, index) => (
+                <Timer
+                key={time.id}
+                // not sure on above
+                time={time}
+                index={index}
+                startTimer={this.startTimer}
+                startNextTimer={this.startNextTimer}
+                handleTimer={this.handleTimer}
+                handleReset={this.handleReset}
+                activeTimer={this.state.activeTimer}
+                isRunning={this.state.isRunning}
+                />
+            ))}
+            </article>
+        </section>
+
         <section>
             {/* going to filter over all PracticeSessionCards then map over Practice Cards */}
             {   this.props.Sessions.filter(session =>
---
+++ src/components/Timer/Timer.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+// import "./Stopwatch.css";
+
+export default class Timer extends Component {
+
+  componentDidMount() {
+  }
+
+  parseTime = () => {let totalTime = this.props.time.elapsedTime;
+    let hrs = ~~(totalTime / 3600);
+    let mins = ~~((totalTime % 3600) / 60);
+    let secs = ~~totalTime % 60;
+
+    // Output like "1:01" or "4:03:59" or "123:03:59"
+    let ret = "";
+
+    if (hrs > 0) {
+        ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
+    }
+
+    ret += "" + mins + ":" + (secs < 10 ? "0" : "");
+    ret += "" + secs;
+    return ret;
+  }
+
+  render() {
+
+
+    return (
+      <div key={this.props.time.id} className="timer">
+        <h4>CountDown Timer</h4>
+        <span className="timer-time">{<h1 className={`${this.props.isRunning ? "fade" : ""}`}>{this.parseTime()}</h1>}</span>
+        <br />
+        <button
+          onClick={() =>
+            this.props.handleTimer(this.props.index, this.props.time)
+          }
+          type="button"
+          className="btn btn-secondary btn-sm stopwatch_button start"
+        >
+          {this.props.isRunning && this.props.activeTimer === this.props.index
+            ? "Pause"
+            : "Start"}
+        </button>
+        <button
+        disabled={this.props.activeTimer === null}
+          onClick={this.props.handleReset}
+          type="button"
+          className={`${"btn btn-secondary btn-sm stopwatch_button reset"}`}
+       >
+          Reset
+        </button>
+      </div>
+    );
+  }
+}
